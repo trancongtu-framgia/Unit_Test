15febcb7a5d8900f7223c6f45a3d7d97258df3b6
2751997nam <nambcvt275@gmail.com>
2018-10-05 13:20:43 +0700

    teams + type API

---
+++ app/Http/Controllers/TeamController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Team;
+use Validator;
+use Illuminate\Http\Request;
+use App\Http\Requests\TeamRequest;
+
+class TeamController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(Team::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(TeamRequest $request)
+    {
+        Team::create(
+            $request->only(
+                'name'
+            )
+        );
+
+        return response()->json(['message' => config('api.create')]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return response()->json(Team::findOrFail($id));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(TeamRequest $request, $id)
+    {
+        return Team::updateTeam(
+            $id,
+            $request->only(
+                'name'
+            )
+        );
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Team::destroy($id);
+    }
+}
--- app/Models/Team.php
+++ app/Models/Team.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Team extends Model
 {
     protected $guarded = ['id'];
+
+    public function batches()
+    {
+        return $this->hasMany('App\Batch');
+    }
+
+    public function scopeUpdateTeam($query, $id, $data)
+    {
+        $team = Team::findOrFail($id);
+        $team->update($data);
+
+        return response()->json(['message' => config('api.update')]);
+    }
 }
